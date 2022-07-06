bb3b51190663994bef82f8408874675e0f40f882
galaxy-iot <wh8199@foxmail.com>
2022-07-06 08:25:54 +0800

    add submodule azure-iot-sdk-c

--- Makefile
+++ Makefile
@@ -1,7 +1,5 @@
-force:
-
-protobuf: force
+protobuf:
 	protoc --cpp_out=./src --grpc_out=./src   --plugin=protoc-gen-grpc=/home/wh8199/grpc/cmake/build/grpc_cpp_plugin  ./protobuf/grpc_server.proto
 
-aa:
-	echo ${CURDIR}
+clean:
+	rm -rf build
--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,33 @@
 #include <protobuf/grpc_server.pb.h>
 #include "server/server.h"
 
-int main() {
-    std::cout << "aaa" << std::endl;
-    std::string server_address("0.0.0.0:50051");
+#include "iothub.h"
+#include "iothub_device_client_ll.h"
+#include "iothub_client_options.h"
+#include "iothub_message.h"
+#include "azure_c_shared_utility/threadapi.h"
+#include "azure_c_shared_utility/crt_abstractions.h"
+#include "azure_c_shared_utility/shared_util_options.h"
+#include "iothubtransportmqtt.h"
 
-    std::cout << server_address << std::endl;
+int main() {
+    IOTHUB_CLIENT_TRANSPORT_PROVIDER protocol;
+    IOTHUB_MESSAGE_HANDLE message_handle;
+    size_t messages_sent = 0;
+    const char* telemetry_msg = "test_message";
+    printf("%s\n",telemetry_msg);
+    protocol = MQTT_Protocol;
+
+    (void)IoTHub_Init();
+    IOTHUB_DEVICE_CLIENT_LL_HANDLE device_ll_handle;
+
+    (void)printf("Creating IoTHub Device handle\r\n");
+    // Create the iothub handle here
+    device_ll_handle = IoTHubDeviceClient_LL_CreateFromConnectionString("", protocol);
+    if (device_ll_handle == NULL)
+    {
+        (void)printf("Failure creating IotHub device. Hint: Check your connection string.\r\n");
+    }
 
     CustomServer *s = new CustomServer(9999);
 
